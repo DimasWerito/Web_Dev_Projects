100d46956452c1a51a4f1175c654e11e7ba4ff1d
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-08-14 11:55:12 +0200

    love calculator new

--- loveCalculator/script.js
+++ loveCalculator/script.js
@@ -1,8 +1,18 @@
 const nameOne = prompt("What is HIS name?");
 const nameTwo = prompt("What is HER name?");
 
-let n = Math.random();
-n = n * 100;
-n = Math.floor(n) + 1;
+let loveScore = Math.random();
+loveScore = loveScore * 100;
+loveScore = Math.floor(loveScore) + 1;
 
-alert(`Your love match is ${n}%`);
+if (loveScore > 70) {
+  alert(
+    `Your love match is ${loveScore}%. You love each other like Kanye loves Kanye!`
+  );
+} else if (loveScore > 30 && loveScore <= 70) {
+  alert(`Your love match is ${loveScore}%`);
+} else {
+  alert(
+    `Your love match is ${loveScore}%. You go together like oil and water.`
+  );
+}
