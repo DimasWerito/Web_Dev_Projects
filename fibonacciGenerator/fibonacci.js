458aad4c863e844019177ff6d545cb50558d34e0
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-08-14 18:00:49 +0200

    fibonacci

---
+++ fibonacciGenerator/fibonacci.js
@@ -0,0 +1,25 @@
+function fibonacciGenerator(n) {
+  let fibonacciSequence = [];
+
+  // Перевірка для випадку, коли n дорівнює 0
+  if (n === 0) {
+    return fibonacciSequence;
+  }
+
+  // Додаємо перше число Фібоначчі - 0
+  fibonacciSequence.push(0);
+
+  // Якщо потрібно більше одного числа, додаємо друге число Фібоначчі - 1
+  if (n > 1) {
+    fibonacciSequence.push(1);
+  }
+
+  // Генеруємо решту чисел Фібоначчі
+  for (let i = 2; i < n; i++) {
+    let nextNumber = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+    fibonacciSequence.push(nextNumber);
+  }
+
+  // Повертаємо масив чисел Фібоначчі
+  return fibonacciSequence;
+}
