bce116fe83b16a883b8d56f1526d1bd0f2b6284a
Dmytro Krasilnykov <krasilnykov@gmail.com>
2024-08-14 11:43:05 +0200

    love calculator

---
+++ loveCalculator/script.js
@@ -0,0 +1,8 @@
+const nameOne = prompt("What is HIS name?");
+const nameTwo = prompt("What is HER name?");
+
+let n = Math.random();
+n = n * 100;
+n = Math.floor(n) + 1;
+
+alert(`Your love match is ${n}%`);
